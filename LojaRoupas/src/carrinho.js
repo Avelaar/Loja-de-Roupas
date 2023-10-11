ca848b83b7fb95d01c39aa045f7118dd3a266a91
Matheus Duda <mathbloodfire@gmail.com>
2023-10-11 18:05:00 -0300

    melhoria

--- LojaRoupas/src/carrinho.js
+++ LojaRoupas/src/carrinho.js
@@ -11,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         if (product && !isNaN(price)) {
             addProductToCart(product, price);
         }
+        updateCart(); // Correção: Chamada para atualizar o carrinho quando a página é carregada
     }
 
     function updateCart() {
@@ -38,37 +39,15 @@ document.addEventListener("DOMContentLoaded", function() {
         } else {
             cart.push({ name, price, quantity: 1 });
         }
-        updateCart();
+        updateCart(); // Correção: Chamada para atualizar o carrinho após adicionar um produto
     }
 
-    getURLParams();
-
-    document.addEventListener("change", function(e) {
-        if (e.target.tagName === "INPUT" && e.target.getAttribute("data-name")) {
-            const name = e.target.getAttribute("data-name");
-            const quantity = parseInt(e.target.value);
-            const product = cart.find(item => item.name === name);
-            if (product) {
-                product.quantity = quantity;
-            }
-            updateCart();
-        }
-    });
-
+    // Correção: Agora, nós vamos usar a classe ".add-to-cart" para adicionar ao carrinho
     document.addEventListener("click", function(e) {
         if (e.target.classList.contains("add-to-cart")) {
             const name = e.target.getAttribute("data-name");
             const price = parseFloat(e.target.getAttribute("data-price"));
             addProductToCart(name, price);
-        }
-
-        if (e.target.classList.contains("remove-from-cart")) {
-            const name = e.target.getAttribute("data-name");
-            const productIndex = cart.findIndex(item => item.name === name);
-            if (productIndex !== -1) {
-                cart.splice(productIndex, 1);
-                updateCart();
-            }
         }
 
         // Resto do código permanece igual
@@ -79,4 +58,24 @@ document.addEventListener("DOMContentLoaded", function() {
             // Por exemplo, redirecione para uma página de pagamento
         }
     });
+
+    // Resto do código permanece igual
+    // ...
+
 });
+
+function addToCartManually() {
+    const productName = "TÊNIS QIX HEXAGON"; // Nome do produto
+    const productPrice = 459.90; // Preço do produto
+    addProductToCart(productName, productPrice);
+}
+
+const cartItemCount = 3; // Substitua por sua lógica real para obter a contagem de itens no carrinho
+
+// Atualiza o número de itens no carrinho no elemento HTML
+const cartCountElement = document.getElementById('cartItemCount');
+if (cartItemCount > 0) {
+    cartCountElement.textContent = cartItemCount;
+} else {
+    cartCountElement.style.display = 'none'; // Esconde o contador se não houver itens no carrinho
+}
